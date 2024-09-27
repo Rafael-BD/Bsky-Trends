82dfc64e31b2d82b26e84222610269d061accbac
Rafael-BD <rafael8885326@gmail.com>
2024-09-26 21:22:21 -0300

    feat: refactor WebSocket client connection to return a promise and handle errors more effectively

--- src/postsListening.ts
+++ src/postsListening.ts
@@ -17,61 +17,63 @@ import { extractHashtags } from "./trends/hashtags.ts";
  * from the Bsky network and update the sketches with the
  * extracted ngrams (words, phrases and hashtags).
  */
-async function createWebSocketClient() {
-    async function connectToWebSocket() {
-        try {
-            const client = subscribeRepos(`wss://bsky.network`, { decodeRepoOps: true })
-            client.on('message', (m: SubscribeReposMessage) => {
-                if (ComAtprotoSyncSubscribeRepos.isCommit(m)) {
-                    m.ops.forEach((op) => {
-                        if (!op?.payload) return;
-                        const payload = op.payload as { $type: string; text?: string; langs?: string[]; createdAt: string; };
-                        if (!payload.langs) return;
-                        if (payload.$type !== "app.bsky.feed.post" || !payload.text) return;
+function createWebSocketClient(): Promise<void> {
+    return new Promise((reject) => {
+        async function connectToWebSocket() {
+            try {
+                const client = subscribeRepos(`wss://bsky.network`, { decodeRepoOps: true });
+                client.on('message', (m: SubscribeReposMessage) => {
+                    if (ComAtprotoSyncSubscribeRepos.isCommit(m)) {
+                        m.ops.forEach((op) => {
+                            if (!op?.payload) return;
+                            const payload = op.payload as { $type: string; text?: string; langs?: string[]; createdAt: string; };
+                            if (!payload.langs) return;
+                            if (payload.$type !== "app.bsky.feed.post" || !payload.text) return;
 
-                        const postDate = new Date(payload.createdAt);
-                        const currentDate = new Date();
-                        const diff = currentDate.getTime() - postDate.getTime();
-                        const diffInHours = diff / (1000 * 60 * 60); 
-                        if (diffInHours > 12) return; // Ignore posts older than 12 hours
+                            const postDate = new Date(payload.createdAt);
+                            const currentDate = new Date();
+                            const diff = currentDate.getTime() - postDate.getTime();
+                            const diffInHours = diff / (1000 * 60 * 60); 
+                            if (diffInHours > 12) return; // Ignore posts older than 12 hours
 
-                        const lang = payload.langs[0];
-                        if(lang !== 'pt' && lang !== 'en') return;
+                            const lang = payload.langs[0];
+                            if(lang !== 'pt' && lang !== 'en') return;
 
-                        const txt = payload.text.trim();
+                            const txt = payload.text.trim();
+                        
+                            const processedText = preProcessText(txt);
+                            const date = new Date(payload.createdAt);
+
+                            if (processedText.split(" ").every(word => stopwords_pt.has(word))) return; // Ignore posts with only stopwords
+
+                            // Extract phrases, words and hashtags
+                            const phrases = filterSentences(extractSentences(processedText));
+                            const words = filterWords(extractWords(processedText), lang);
+                            const hashtags = filterWords(extractHashtags(processedText), lang);
+                            updateSketches({ words, phrases, hashtags }, date, lang as 'pt' | 'en');
                     
-                        const processedText = preProcessText(txt);
-                        const date = new Date(payload.createdAt);
+                        });
+                    }
+                });
 
-                        if (processedText.split(" ").every(word => stopwords_pt.has(word))) return; // Ignore posts with only stopwords
+                client.on('error', (e) => {
+                    console.error('Erro na conexão WebSocket:', e);
+                    setTimeout(() => connectToWebSocket(), 5000);
+                });
 
-                        // Extract phrases, words and hashtags
-                        const phrases = filterSentences(extractSentences(processedText));
-                        const words = filterWords(extractWords(processedText), lang);
-                        const hashtags = filterWords(extractHashtags(processedText), lang);
-                        updateSketches({ words, phrases, hashtags }, date, lang as 'pt' | 'en');
-                
-                    });
-                }
-            })
-
-            client.on('error', (e) => {
-                console.error('Erro na conexão WebSocket:', e);
+                client.on('close', () => {
+                    console.error('Conexão WebSocket fechada');
+                    setTimeout(() => connectToWebSocket(), 1000);
+                });
+            } catch (error) {
+                console.error('Erro na conexão WebSocket:', error);
+                reject(error); // Reject the promise on error
                 setTimeout(() => connectToWebSocket(), 5000);
-            })
-
-            client.on('close', () => {
-                console.error('Conexão WebSocket fechada');
-                setTimeout(() => connectToWebSocket(), 1000);
-            })
-        } catch (error) {
-            console.error('Erro na conexão WebSocket:', error);
-            setTimeout(() => connectToWebSocket(), 5000);
+            }
         }
-    }
 
-    await connectToWebSocket();
+        connectToWebSocket();
+    });
 }
-
 
 export { createWebSocketClient };
